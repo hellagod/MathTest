5eaadfd8524476af51ad3ab1f3753d5415fa3ff1
Samathpyth <kriss.sacha@gmail.com>
2020-05-07 12:18:40 +0300

    add start frontend - 2

---
+++ frontend/build/precache-manifest.1111190e80f426ffa7ed0ab78815e561.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb80f1a338cc4ab1d590683444e0935",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c3adc26c659542c4060",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "23ce55a90b564c14bacd",
+    "url": "/static/js/2.fee698fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fee698fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3adc26c659542c4060",
+    "url": "/static/js/main.cfd20b22.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
