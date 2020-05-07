f486842f254d295e7c4cc521c7d112d9825e750e
Samathpyth <kriss.sacha@gmail.com>
2020-05-07 10:43:02 +0300

    12345

---
+++ frontend/build/precache-manifest.8bb03ff66fb040fb24a685444ab80cac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f53abb6cbd8b0558aeedb79a7d4c228",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8b3f71da9d8b5dc45fa",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ca96cb1d2607882b95b5",
+    "url": "/static/js/2.9a317882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9a317882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b3f71da9d8b5dc45fa",
+    "url": "/static/js/main.56cd2ec5.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
